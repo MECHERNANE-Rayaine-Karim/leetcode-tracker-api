029062ed36fb4b4c69cb173366ed390a152a3da4
MECHERNANE-Rayaine-Karim <mechernanerk@gmail.com>
2026-07-16 06:13:43 +0100

    Add problem_topics junction table for many-to-many Problem-Topic relationship

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from app.core.config import settings
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-
+from sqlalchemy import Table, Column, ForeignKey
 
 engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(bind=engine)
@@ -16,3 +16,11 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+problem_topics = Table(
+    "problem_topics",
+    Base.metadata,
+    Column("topic_id", ForeignKey("topics.id"), primary_key=True ),
+    Column("problem_id", ForeignKey("problems.id"), primary_key=True )
+)
